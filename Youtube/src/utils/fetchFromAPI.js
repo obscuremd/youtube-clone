b8e63d4c902c964c61a5ea19a248177376aa6f04
dan <md.erhenede@gmail.com>
2023-11-01 07:35:27 +0100

    first commit

---
+++ Youtube/src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': '9af62694d7msh3eb365c5babf97ep17c066jsndf09d2c13008',
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
